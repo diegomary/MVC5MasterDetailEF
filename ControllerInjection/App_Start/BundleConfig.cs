65a93c23a03b6c9c83b8a517de150ad57808902a
diego <diego@dmm888.com>
2014-04-12 16:29:00 +0100

    Final Stage of Clear style

--- ControllerInjection/App_Start/BundleConfig.cs
+++ ControllerInjection/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace ControllerInjection
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.IgnoreList.Clear();
+          
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryUnobtrusive").Include(
